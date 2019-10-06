9b8b649399de52a90ad6a84115ef7c45c9fc57d8
Margarida Dinis <margarida.tds@gmail.com>
2019-10-06 20:01:01 +0200

    Add image upload with cloudinary

--- frontend/components/organisms/CreateAlbum/CreateAlbum.js
+++ frontend/components/organisms/CreateAlbum/CreateAlbum.js
@@ -7,7 +7,7 @@ import Input from '../../atoms/Input';
 
 import { CREATE_ALBUM_MUTATION } from './index';
 
-const initialState = { name: 'Name', year: 0, description: '', image: 'sdf', largeImage: 'df' };
+const initialState = { name: 'Name', year: 0, description: '', image: '', largeImage: '' };
 
 const CreateAlbum = () => {
   const [values, setValues] = useState(initialState);
@@ -18,17 +18,48 @@ const CreateAlbum = () => {
     setValues({ ...values, [name]: val });
   };
 
+  const handleUploadFile = async({ target: { files } }) => {
+    const [uploadedFile] = files;
+    if (!uploadedFile) {
+      setValues({ ...values, image: '', largeImage: '' });
+      return;
+    }
+
+    const data = new FormData();
+    data.append('file', uploadedFile);
+    data.append('upload_preset', 'albums');
+
+    const response = await fetch(
+      'https://api.cloudinary.com/v1_1/margaridadinis/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    );
+
+    const file = await response.json();
+
+    setValues({
+      ...values,
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
+  const handleSubmit = async(e, createAlbum) => {
+    e.preventDefault();
+    const res = await createAlbum();
+
+    Router.push({
+      pathname: '/album',
+      query: { id: res.data.createAlbum.id }
+    })
+  };
+
   return (
     <Mutation mutation={CREATE_ALBUM_MUTATION} variables={values}>
       {(createAlbum, { loading, error }) => (
-        <Form onSubmit={async e => {
-          e.preventDefault();
-          const res = await createAlbum();
-          Router.push({
-            pathname: '/album',
-            query: { id: res.data.createAlbum.id }
-          })
-        }}>
+        <Form onSubmit={ e => handleSubmit(e, createAlbum)}>
           {error && <ErrorMessage error={error} />}
           <fieldset disabled={loading} aria-busy={loading}>
             <Input
@@ -53,14 +84,15 @@ const CreateAlbum = () => {
               handleChange={handleChange}
             />
             <Input
+              type='file'
               name='image'
               label='Album cover'
-              value={values.image}
-              handleChange={handleChange}
+              handleChange={handleUploadFile}
               required
             />
+            {values.image && <img src={values.image} alt='Uploaded Image' width='200'/>}
           </fieldset>
-          <button type='submit'>Submit</button>
+          <button type='submit' disabled={!values.image}>Submit</button>
         </Form>
       )
       }
