5f61e93a4b089100156daa6ca0ccfc714aa10724
Margarida Dinis <margarida.tds@gmail.com>
2019-10-18 21:09:26 +0200

    feat(signup): improve user type and add signup mutation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 require('dotenv');
+const cookieParser = require('cookie-parser');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO use express middleware to populate current user
 
 server.start({
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
   createAlbum(parent, args, context, info) {
     return context.db.mutation.createAlbum({
@@ -19,7 +22,27 @@ const mutations = {
     const item = await context.db.query.album({ where }, `{ id name }`);
 
     return context.db.mutation.deleteAlbum({ where }, info);
-  }
+  },
+  async signUp(parent, args, context, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await context.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    context.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    return user;
+  },
 };
 
 module.exports = mutations;
