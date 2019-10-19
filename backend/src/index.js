48401cec9d6331859c89431cc54159b2ff94e14a
Margarida Dinis <margarida.tds@gmail.com>
2019-10-19 17:41:26 +0200

    feat(user): get the user id in each request and add me query

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 require('dotenv');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO use express middleware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,6 +4,13 @@ const Query = {
   albums: forwardTo('db'),
   album: forwardTo('db'),
   albumsConnection: forwardTo('db'),
+  me(parent, args, context, info) {
+    const { userId } = context.request;
+
+    if (!userId) return null;
+
+    return context.db.query.user({ where: { id: userId } }, info);
+  },
 };
 
 module.exports = Query;
