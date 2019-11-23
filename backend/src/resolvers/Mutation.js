e6c9952741078f320120dd9e50aba34a9e58fab5
Margarida Dinis <margarida.tds@gmail.com>
2019-11-23 17:33:34 +0100

    revert(cart): remove all cart related schemas and mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -151,41 +151,6 @@ const mutations = {
       info,
     );
   },
-  async addToCart(parent, args, ctx, info) {
-    const { userId } = ctx.request;
-    if (!userId) throw new Error('You must be logged in!');
-
-    const [existingCartItem] = await ctx.db.query.cartItems({
-      where: {
-        user: { id: userId },
-        album: { id: args.id },
-      },
-    });
-
-    if (existingCartItem) {
-      return ctx.db.mutation.updateCartItem({
-        where: { id: existingCartItem.id },
-        data: { quantity: existingCartItem.quantity + 1 },
-      }, info);
-    }
-
-    return ctx.db.mutation.createCartItem({
-      data: {
-        user: { connect: { id: userId } },
-        album: { connect: { id: args.id } },
-      },
-    }, info);
-  },
-  async removeFromCart(parent, args, ctx, info) {
-    const { userId } = ctx.request;
-
-    const cartItem = await ctx.db.query.cartItem({ where: { id: args.id } }, '{ id user { id } }');
-
-    if (!cartItem) throw new Error('No item found');
-    if (cartItem.user.id !== userId) throw new Error('This item is not on your cart');
-
-    return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
-  },
 };
 
 module.exports = mutations;
